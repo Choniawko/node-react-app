906092032cfe5b3c2ff3c75ddb45adf93d41f876
choniawko <pawel.choniawko@gmail.com>
2017-03-14 22:07:50 +0100

    router install

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+export const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/new">New</Link>
+    <Link to="/index">Index</Link>
+  </nav>
+)
 
 class App extends Component {
   render() {
@@ -10,9 +19,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+          <Links />
+            <Route exact path="/" render={() => {
+              return <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>}
+            } />
+            <Route path="/new" render={() => <p>Add new element</p>} />
+            <Route path="/index" render={() => <p>index elements</p>} />
+          </div>
+        </Router>
+        
       </div>
     );
   }
